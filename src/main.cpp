6c079cd5940051d085c38d86dcfa88a042b8a7a0
Timber <redwoodfox479@gmail.com>
2025-02-14 05:45:05 -0600

    Added unhandled exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,50 @@
 #include "terminal.h"
 
-#include <iostream>
+#include <cstdlib>
+#include <exception>
+#include <string>
 
+// TODO: Remove once reading console input is implemented.
 #include <unistd.h>
+
+
+
+void logUnhandledException(const std::string& exceptionMsg);
 
 
 
 int main(int argc, char* argv[])
 {
-    Cedar::Terminal::setMode(Cedar::Terminal::Mode::raw);
-    Cedar::Terminal::enableAltScreenBuffer(true);
+    int exitStatus = EXIT_FAILURE;
 
-    Cedar::Terminal::writeLine("Hello world!");
-    std::cout << "Making sure standard output works still" << std::endl;
+    try
+    {
+        Cedar::Terminal::setMode(Cedar::Terminal::Mode::raw);
+        Cedar::Terminal::enableAltScreenBuffer(true);
+        Cedar::Terminal::writeLine("Hello world! \u256B press 'Q' to quit");
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && !(c == 'q' || c == 'Q'));
-    
-    Cedar::Terminal::setMode(Cedar::Terminal::Mode::cooked);
-    
-    return 0;
+        // TODO: Remove once reading console input is implemented.
+        char c;
+        while (read(STDIN_FILENO, &c, 1) == 1 && !(c == 'q' || c == 'Q'));
+
+        exitStatus = EXIT_SUCCESS;
+    }
+    catch(const std::exception& e)
+    {
+        logUnhandledException(e.what());
+    }
+    catch (...)
+    {
+        logUnhandledException("An unknown exception occurred");
+    }
+
+    return exitStatus;
+}
+
+
+
+void logUnhandledException(const std::string& exceptionMsg)
+{
+    Cedar::Terminal::enableAltScreenBuffer(false);
+    Cedar::Terminal::writeLine("Unhandled exception: " + exceptionMsg);
 }
