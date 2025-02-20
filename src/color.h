19039737276157263002028681299780b00a3473
Timber <redwoodfox479@gmail.com>
2025-02-20 04:27:00 -0600

    Moved Cedar::Terminal::Color to Cedar::Color

---
+++ src/color.h
@@ -0,0 +1,30 @@
+//
+// Color.
+//
+
+#ifndef CEDAR_COLOR_H
+#define CEDAR_COLOR_H
+
+namespace Cedar
+{
+    enum class Color {
+        black          = 30,
+        red            = 31,
+        green          = 32,
+        yellow         = 33,
+        blue           = 34,
+        magenta        = 35,
+        cyan           = 36,
+        white          = 37,
+        bright_black   = 90,
+        bright_red     = 91,
+        bright_green   = 92,
+        bright_yellow  = 93,
+        bright_blue    = 94,
+        bright_magenta = 95,
+        bright_cyan    = 96,
+        bright_white   = 97,
+    };
+}
+
+#endif // CEDAR_COLOR_H
