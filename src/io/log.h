d9f32e12c85d7d7997140b5110d5209bfa39477f
Timber Pines <redwoodfox479@gmail.com>
2025-05-14 19:33:16 -0500

    Added log macros

--- src/io/log.h
+++ src/io/log.h
@@ -13,6 +13,10 @@
 #define CEDAR_LOG_LEVEL_CRITICAL 4
 #define CEDAR_LOG_LEVEL_FATAL    5
 
+#if !defined(CEDAR_LOG_MIN_LEVEL)
+    #define CEDAR_LOG_MIN_LEVEL CEDAR_LOG_LEVEL_DEBUG
+#endif
+
 
 
 namespace Cedar::Log
@@ -104,5 +108,43 @@ namespace Cedar::Log
         message(Level::fatal, msg);
     }
 }
+
+
+
+#if CEDAR_LOG_MIN_LEVEL <= CEDAR_LOG_LEVEL_DEBUG
+    #define CEDAR_LOG_DEBUG(msg) Cedar::Log::debug(msg)
+#else
+    #define CEDAR_LOG_DEBUG(msg) ((void)0)
+#endif
+
+#if CEDAR_LOG_MIN_LEVEL <= CEDAR_LOG_LEVEL_INFO
+    #define CEDAR_LOG_INFO(msg) Cedar::Log::info(msg)
+#else
+    #define CEDAR_LOG_INFO(msg) ((void)0)
+#endif
+
+#if CEDAR_LOG_MIN_LEVEL <= CEDAR_LOG_LEVEL_WARNING
+    #define CEDAR_LOG_WARNING(msg) Cedar::Log::warning(msg)
+#else
+    #define CEDAR_LOG_WARNING(msg) ((void)0)
+#endif
+
+#if CEDAR_LOG_MIN_LEVEL <= CEDAR_LOG_LEVEL_ERROR
+    #define CEDAR_LOG_ERROR(msg) Cedar::Log::error(msg)
+#else
+    #define CEDAR_LOG_ERROR(msg) ((void)0)
+#endif
+
+#if CEDAR_LOG_MIN_LEVEL <= CEDAR_LOG_LEVEL_CRITICAL
+    #define CEDAR_LOG_CRITICAL(msg) Cedar::Log::critical(msg)
+#else
+    #define CEDAR_LOG_CRITICAL(msg) ((void)0)
+#endif
+
+#if CEDAR_LOG_MIN_LEVEL <= CEDAR_LOG_LEVEL_FATAL
+    #define CEDAR_LOG_FATAL(msg) Cedar::Log::fatal(msg)
+#else
+    #define CEDAR_LOG_FATAL(msg) ((void)0)
+#endif
 
 #endif // CEDAR_IO_LOG_H
