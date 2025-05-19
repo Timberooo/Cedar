b3530a9b55a09f67b44fe0e6902fcb284f068cac
Timber <redwoodfox479@gmail.com>
2025-05-19 10:12:37 -0500

    Removed old commented-out macros and fixed macro token-pasting operator errors on Linux

--- src/nifty_counter.h
+++ src/nifty_counter.h
@@ -17,15 +17,15 @@ private:                                        \
     static std::size_t s_counter;               \
 };                                              \
                                                 \
-static counterName g_##counterName##;
+static counterName g_##counterName;
 
 
 
 #define CEDAR_NIFTY_COUNTER_INTERNAL(internalStructName, counterName, counterNamespace)                                   \
 namespace                                                                                                                 \
 {                                                                                                                         \
-    static typename std::aligned_storage<sizeof(counterNamespace::counterName), alignof(counterNamespace::counterName)>::type g_##internalStructName##Buffer; \
-    internalStructName& g_##internalStructName## = reinterpret_cast<internalStructName&>(g_##internalStructName##Buffer); \
+    static typename std::aligned_storage<sizeof(counterNamespace::counterName), alignof(counterNamespace::counterName)>::type g_##internalStructNameBuffer; \
+    internalStructName& g_##internalStructName = reinterpret_cast<internalStructName&>(g_##internalStructNameBuffer); \
 }                                                                                                                         \
                                                                                                                           \
 namespace counterNamespace                                                                                                \
@@ -35,7 +35,7 @@ namespace counterNamespace                                                      
     counterName::counterName()                                                                                            \
     {                                                                                                                     \
         if (s_counter == 0)                                                                                               \
-            new (&g_##internalStructName##)internalStructName();                                                          \
+            new (&g_##internalStructName)internalStructName();                                                          \
                                                                                                                           \
         s_counter++;                                                                                                      \
     }                                                                                                                     \
@@ -45,119 +45,8 @@ namespace counterNamespace                                                      
         s_counter--;                                                                                                      \
                                                                                                                           \
         if (s_counter == 0)                                                                                               \
-            g_##internalStructName##.~internalStructName();                                                               \
+            g_##internalStructName.~internalStructName();                                                               \
     }                                                                                                                     \
 }
 
-
-
-//#define CEDAR_NIFTY_COUNTER_INTERNAL(internalStructName, counterName) \
-//namespace \
-//{ \
-//    static typename std::aligned_storage<sizeof(counterName), alignof(counterName)>::type g_##internalStructName##Buffer; \
-//    internalStructName& g_##internalStructName## = reinterpret_cast<internalStructName&>(g_##internalStructName##Buffer); \
-//} \
-//\
-//std::size_t counterName
-//
-//
-//
-//#define CEDAR_NIFTY_COUNTER_INTERNAL_WITH_NAMESPACE(internalStructName, counterName, counterNamespace) CEDAR_NIFTY_COUNTER_INTERNAL(internalStructName, counterNamespace::counterName)
-
 #endif // CEDAR_NIFTY_COUNTER_H
-
-
-
-
-
-//#ifndef CEDAR_NIFTY_COUNTER_H
-//#define CEDAR_NIFTY_COUNTER_H
-//
-//#include <cstddef>
-//#include <new>
-//#include <type_traits>
-//
-//
-//
-//#define CEDAR_NIFTY_COUNTER_HEADER(initializerName) \
-//class initializerName                               \
-//{                                                   \
-//public:                                             \
-//    initializerName();                              \
-//    ~initializerName();                             \
-//private:                                            \
-//    static std::size_t s_counter;                   \
-//};                                                  \
-//                                                    \
-//static initializerName g_initializerName;
-//
-//
-//
-//#define CEDAR_NIFTY_COUNTER_INTERNAL(initializerName, internalStructName) \
-//namespace \
-//{ \
-//    static typename std::aligned_storage<sizeof(initializerName), alignof(initializerName)>::type g_##internalStructName##Buffer; \
-//    internalStructName& g_internalStructName = reinterpret_cast<internalStructName&>(g_##internalStructName##Buffer); \
-//}
-//
-//
-//
-//#define CEDAR_NIFTY_COUNTER_INTERNAL_WITH_NAMESPACE(initializerName, internalStructName, initializerNamespace) CEDAR_NIFTY_COUNTER_INTERNAL(initializerNamespace::initializerName, internalStructName)
-//
-//#endif // CEDAR_NIFTY_COUNTER_H
-
-
-
-
-
-//#ifndef CEDAR_NIFTY_COUNTER_H
-//#define CEDAR_NIFTY_COUNTER_H
-//
-//#include <cstddef>
-//#include <new>
-//#include <type_traits>
-//
-//
-//
-//#define CEDAR_NIFTY_COUNTER_HEADER(initializerName) \
-//class initializerName                               \
-//{                                                   \
-//public:                                             \
-//    initializerName();                              \
-//    ~initializerName();                             \
-//private:                                            \
-//    static std::size_t s_counter;                   \
-//};                                                  \
-//static initializerName g_initializerName;
-//
-//
-//
-//#define CEDAR_NIFTY_COUNTER_INTERNAL(initializerName, initializerNamespace, internalDataStructName)                                                                           \
-//namespace                                                                                                                                                                     \
-//{                                                                                                                                                                             \
-//    static typename std::aligned_storage<sizeof(initializerNamespace::initializerName), alignof(initializerNamespace::initializerName)>::type g_internalDataStructNameBuffer; \
-//    internalDataStructName& g_internalDataStructName = reinterpret_cast<internalDataStructName&>(g_internalDataStructNameBuffer);                                             \
-//}                                                                                                                                                                             \
-//                                                                                                                                                                              \
-//namespace initializerNamespace                                                                                                                                                \
-//{                                                                                                                                                                             \
-//    std::size_t initializerName::s_counter = 0;                                                                                                                               \
-//                                                                                                                                                                              \
-//    initializerName::initializerName()                                                                                                                                        \
-//    {                                                                                                                                                                         \
-//        if (s_counter == 0)                                                                                                                                                   \
-//            new (&g_internalDataStructName)internalDataStructName();                                                                                                          \
-//                                                                                                                                                                              \
-//        s_counter++;                                                                                                                                                          \
-//    }                                                                                                                                                                         \
-//                                                                                                                                                                              \
-//    initializerName::~initializerName()                                                                                                                                       \
-//    {                                                                                                                                                                         \
-//        s_counter--;                                                                                                                                                          \
-//                                                                                                                                                                              \
-//        if (s_counter == 0)                                                                                                                                                   \
-//            g_internalDataStructName.~internalDataStructName();                                                                                                               \
-//    }                                                                                                                                                                         \
-//}
-//
-//#endif // CEDAR_NIFTY_COUNTER_H
