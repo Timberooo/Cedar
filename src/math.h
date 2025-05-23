dcd17d97a3240b419fa149da0d3ff421d634093b
Timber Pines <redwoodfox479@gmail.com>
2025-05-22 20:36:10 -0500

    Added math.h

---
+++ src/math.h
@@ -0,0 +1,13 @@
+//
+// A collection of header files located in the "math" directory.
+//
+
+#ifndef CEDAR_MATH_H
+#define CEDAR_MATH_H
+
+#include "math/math_common.h"
+#include "math/point.h"
+#include "math/size.h"
+#include "math/vector.h"
+
+#endif // CEDAR_MATH_H
